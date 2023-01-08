fef569bec7d3d8a474f87463b16cc4fdd44b2a56
Bibek <bibekbhusal455@gmail.com>
2023-01-08 17:01:05 +0545

    Cleaned all the unnessary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useeffect, useState } from 'react';
+import autoprefixer from 'autoprefixer';
+import { ImageCard } from './ImageCard';
+import { ImageSearch } from './ImageSearch';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => 
+      res.json())
+    .then(data => {
+      setImages(data.hits);
+      setisLoading(false);
+    })
+    .catch(err => console.log(err));
+  }, [term]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+    
+    {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto">No images</h1>}
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto">Loading...</h1> : <div className="grid grid-cols-3 gap-4" >
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>
+}</div>
   );
 }
 
